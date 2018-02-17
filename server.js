3423d4f29cd1f7d989e492535ebfc77999013589
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-16 16:52:53 -0800

    added module imports

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ var express = require('express'),
     engines = require('consolidate'),
     // module to handle errors
     assert = require('assert'),
-    // require/import the mongodb native drivers.
-    mongodb = require('mongodb');
+    // URI validation module
+    validUrl = require('valid-url'),
+    // require object data modeling
+    mongoose = require('mongoose'),
+    // googl shortener
+    googl = require('goo.gl'),
+    util = require('util');
 
 // serve static files, assets, css, javascript in said directory
 app.use(express.static(__dirname + '/public'));
@@ -23,14 +28,15 @@ app.use(bodyParser.urlencoded({ extended: true}));
 // parse/analyze incoming data as json object
 app.use(bodyParser.json());
 
+// require url-shortener module
+var urlShortenerProvider = require('./public/scripts/url-shortener.js');
 // require routes module
-var routes = require('./public/scripts/routes.js');
-routes(app, mongodb);
+var routes = require('./public/scripts/routes.js')(app, validUrl, urlShortenerProvider, mongoose, googl, util);
 
 // set heroku environment PORT || local PORT
 var port = process.env.PORT || 5000;
 // listen for connection at PORT
-var server = app.listen(port, function(){
+var server = app.listen(port, function() {
   // log the port connection
   console.log("Express server listening on port %s.", port);
 });
